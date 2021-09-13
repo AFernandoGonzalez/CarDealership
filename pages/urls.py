56ec280c8c49356ef61fdd0b06001e830c1b9bea
Fernando Gonzalez <58346389+AFernandoGonzalez@users.noreply.github.com>
2021-09-13 19:39:53 -0400

    all pages completed

--- pages/urls.py
+++ pages/urls.py
@@ -6,4 +6,7 @@ app_name = 'pages'
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('contact/', views.contact, name='contact'),
 ]
